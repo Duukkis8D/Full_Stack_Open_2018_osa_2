1a537c1110a819eed69fd99937432b28c18bcad7
Tuukka Virtanen <duukkis@gmail.com>
2018-10-26 01:03:05 +0300

    Tehtävä 2.12 aluillaan

---
+++ exercise_2.12-2.13/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import axios from 'axios';
+
+class App extends React.Component {
+
+    render() {
+        return (
+            <div>
+                find countries: <input />
+            </div>
+        )
+    }
+
+}
+
+export default App
