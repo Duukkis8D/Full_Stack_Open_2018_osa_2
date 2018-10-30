54c890993eff9f88ac71ce129eac5433befa50f6
Tuukka Virtanen <duukkis@gmail.com>
2018-10-30 05:05:47 +0200

    Tehtävä 2.12 valmis

--- exercise_2.12-2.13/src/App.js
+++ exercise_2.12-2.13/src/App.js
@@ -2,15 +2,71 @@ import React from 'react';
 import axios from 'axios';
 
 class App extends React.Component {
-
-    render() {
-        return (
-            <div>
-                find countries: <input />
-            </div>
-        )
+    constructor(props) {
+        super(props);
+        this.state = {
+            countries: [],
+            search: ''
+        };
     }
 
+    componentDidMount() {
+        axios
+            .get("https://restcountries.eu/rest/v2/all")
+            .then(response => {
+                this.setState({ countries: response.data });
+            });
+    }
+
+    handleSearch = (event) => {
+        this.setState({
+            search: event.target.value
+        })
+    };
+
+    countriesToShow = () => {
+        const countriesToShow =
+            this.state.search === '' ?
+                [] :
+                this.state.countries.filter(
+                    country =>
+                        country.name.toUpperCase().includes( this.state.search.toUpperCase() ) );
+        if (countriesToShow.length > 10) {
+            return (
+                <div>
+                    find countries: <input value={this.state.search} onChange={this.handleSearch}/>
+                    <p>too many matches, specify another filter</p>
+                </div>
+            )
+        }
+        if (countriesToShow.length === 1) {
+            const country = countriesToShow.find(
+                country => country.name.toUpperCase().includes( this.state.search.toUpperCase() ) );
+            return (
+                <div>
+                    find countries: <input value={this.state.search} onChange={this.handleSearch}/>
+                    <h1>{country.name}</h1>
+                    <p>capital: {country.capital}</p>
+                    <p>population: {country.population}</p>
+                    <br></br><img src={country.flag} alt={country.name} width="500"/>
+                </div>
+            )
+        }
+        return (
+            <div>
+                find countries: <input value={this.state.search} onChange={this.handleSearch}/>
+                <ul>
+                    { countriesToShow.map( country =>
+                        <li key={country.name}>{country.name}</li> )
+                    }
+                </ul>
+            </div>
+        )
+    };
+
+    render() {
+        return (<div> {this.countriesToShow()} </div>)
+    }
 }
 
 export default App
